e052a0d2f99af5629bdd44e71f8d26c4e7cafbbb
kirengamartial <kirengamartial@gmail.com>
2024-11-03 18:33:09 +0200

    agri ok

--- src/layouts/ProtectedAdminRoutes.jsx
+++ src/layouts/ProtectedAdminRoutes.jsx
@@ -1,7 +1,6 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
-
 const ProtectedAdminRoutes = () => {
     const {userInfo} = useSelector(state => state.auth)
     return userInfo && userInfo.isAdmin ? <Outlet /> : <Navigate to="/login" />
